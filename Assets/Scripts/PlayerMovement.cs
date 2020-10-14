b4a2e9e6a25a0402a9ad9bad575525e41c566ecf
PixelSergey <sergey.ichtchenko@gmail.com>
2020-10-14 20:54:52 +0300

    Progress the project on day 1
    Lots of stuff done! Audio, art, code, all to make a functional concept game.
    Thanks to William for the base project example.
    TODO: minigame scenes, game layout, menus, progression, other things

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // These are used by the animation system
+    public float mHorizontal;
+    public float mVertical;
+    public float speed;
+    public float scaleX;
+    public float scaleY;
+    public int lastDirection = 1;
+    public float lastScale = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mHorizontal = Input.GetAxis("Horizontal");
+        mVertical = Input.GetAxis("Vertical");
+        
+        if(mHorizontal != 0f && Math.Sign(mHorizontal) != lastDirection && Math.Abs(mHorizontal) > lastScale){
+            transform.localScale = new Vector2(mHorizontal * scaleX, scaleY);
+        }
+        if(Math.Abs(mHorizontal) == 1f){
+            lastDirection = Math.Sign(mHorizontal);
+        }
+        
+        lastScale = Math.Abs(mHorizontal);
+    }
+
+    // All physics operations
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector2 movement = new Vector2(mHorizontal, mVertical);
+        rb.velocity = (movement * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneHandlerExample.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// You will need to write either of these if you wish to write your own code when using UI, or managing a scene
+using UnityEngine.UI; // This allows you to edit UI elements which mostly include presenting: Score/Points, Timer, HealthBar
+using UnityEngine.SceneManagement; // This allows you to control your scene such as Pausing, going to your Main Menu, or reseting a level
+
+public class SceneHandlerExample : MonoBehaviour
+{
+    public Text curState;
+    private bool isPaused;
+    public GameObject pScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pScreen.SetActive(isPaused);
+    }
+
+    public void GO()
+    {
+        StartCoroutine(GameOver());
+    }
+    public void PauseGame()
+    {
+        if(!isPaused)
+        {
+            Time.timeScale = 0f;
+            curState.text = "Paused";
+            //isPaused = true;
+        }
+        else
+        {
+
+            Time.timeScale = 1f;
+            curState.text = "Play";
+            //isPaused = false;
+        }
+        isPaused = !isPaused;
+        pScreen.SetActive(isPaused);
+    }
+    public IEnumerator GameOver()
+    {
+        yield return null;
+        PauseGame();
+        curState.text = "GameOver!";
+        yield return new WaitForSecondsRealtime(2f);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
